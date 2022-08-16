1c29e96022664e0dbd3dd3cf79c6dd360e99e190
Anup <anuppoudel60@gmail.com>
2022-08-16 15:50:04 +1000

    frontend setup done

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./components/assests/Header";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <div>
+        <h1>Hello</h1>
+      </div>
+    </>
+  );
+};
+
+export default App;
---
+++ client/src/components/assests/Header.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <nav className="navbar bg-light mb-4 p-0">
+      <div className="container">
+        <a href="/" className="navbar-brand">
+          <div className="d-flex">
+            <img
+              src="https://images.unsplash.com/photo-1572177812156-58036aae439c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+              className="mr-2"
+              alt="logo"
+            />
+            <div>Project Management</div>
+          </div>
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
